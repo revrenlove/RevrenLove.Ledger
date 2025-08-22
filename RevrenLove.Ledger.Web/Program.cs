1c0fba489e522ea0d44f974c72322fee51dc9247
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-08-22 03:26:43 +0000

    broken, but the right idea

--- RevrenLove.Ledger.Web/Program.cs
+++ RevrenLove.Ledger.Web/Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var ledgerApiClientBaseAddress = builder.Configuration.GetValue<string>("LedgerApiClientBaseAddress")!;
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(ledgerApiClientBaseAddress) });
 
 await builder.Build().RunAsync();
