91696adfd3ae5eb0d447104033db85f9f1534a2e
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-11-04 23:58:04 -0600

    Updating nugets, adding shared project

---
+++ RevrenLove.Ledger.Shared/Frequency.cs
@@ -0,0 +1,15 @@
+﻿namespace RevrenLove.Ledger.Shared;
+
+public enum Frequency
+{
+    None = 0,
+    Daily = 1,
+    Weekly = 2,
+    BiWeekly = 3,
+    SemiMonthly = 4,
+    Monthly = 5,
+    BiMonthly = 6,
+    Quarterly = 7,
+    SemiAnnually = 8,
+    Annually = 9,
+}
