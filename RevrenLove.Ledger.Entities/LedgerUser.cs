f9d222ff9734d82b0ec31e7b9eda300e322bf1f0
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-11-25 22:13:47 -0600

    Removing extra columns

--- RevrenLove.Ledger.Entities/LedgerUser.cs
+++ RevrenLove.Ledger.Entities/LedgerUser.cs
@@ -4,6 +4,5 @@ namespace RevrenLove.Ledger.Entities;
 
 public class LedgerUser : IdentityUser<Guid>
 {
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+
 }
