9d4c8eda3cbbf6fcf570158c73cb18c9026fedda
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-12-18 04:22:44 -0600

    updating database

---
+++ RevrenLove.Ledger.Persistence.SQLite/Migrations/20251218062614_MakeCorrelationIdNullable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RevrenLove.Ledger.Persistence.SQLite.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCorrelationIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
